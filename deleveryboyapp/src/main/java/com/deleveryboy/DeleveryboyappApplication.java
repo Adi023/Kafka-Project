5d73d8f4b03bad3f2f85b6a28aec8081ac4d30f3
Adi023 <adityapandhare050@gmail.com>
2024-10-07 23:43:32 +0530

    feat : Delivery boy app created as producer for kafka

---
+++ deleveryboyapp/src/main/java/com/deleveryboy/DeleveryboyappApplication.java
@@ -0,0 +1,13 @@
+package com.deleveryboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleveryboyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleveryboyappApplication.class, args);
+	}
+
+}
